582e69d78f9ceb1ac564b515d9ae62d8f50b26e3
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2024-10-12 14:59:38 -0300

    Alterando o nome da função usada na rota '/' para corresponder ao CRUD

--- router.js
+++ router.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import home from "./routes/home.js";
 import registerForm from "./routes/register-form.js";
 import createPost from "./routes/create-post.js";
 import deletePost from "./routes/delete-post.js";
+import readPosts from "./routes/read-posts.js";
 
 const router = Router();
-router.get('/', home);
+router.get('/', readPosts);
 router.get('/register', registerForm);
 router.post('/add', createPost);
 router.get('/delete/:id', deletePost);
--- routes/read-posts.js
+++ routes/read-posts.js
@@ -1,6 +1,6 @@
 import Post from "../database/models/Post.js";
 
-export default async function home(req, res) {
+export default async function readPosts(req, res) {
   Post.findAll().then((posts) => {
     res.render('home', { posts: posts.map(post => post.dataValues) });
   }).catch((err) => {
